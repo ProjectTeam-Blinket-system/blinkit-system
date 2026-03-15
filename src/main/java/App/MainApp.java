a1ea87c8a0216140e5d941f5bc998fca03ab97a8
Ashish Boudh <ashishboudh@Mac.lan>
2026-03-15 13:00:33 +0530

    structure

--- src/main/java/App/MainApp.java
+++ src/main/java/App/MainApp.java
@@ -1,2 +1,27 @@
-public class MainApp {
+package com.example.blinkitsystem;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/fxml/home.fxml")
+        );
+
+        Scene scene = new Scene(loader.load(), 600, 400);
+
+        stage.setTitle("Blinkit System");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
